6409c11fbf320bbfe3b9827ac325fca5df35c79f
Álefe Costa Macedo|alefemacedo <almacedobr2@gmail.com>
2023-11-20 20:45:46 -0300

    [master]  - Interface
    - Renaming interface to ui project

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ui-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'interface';
+  title = 'UI';
 }
